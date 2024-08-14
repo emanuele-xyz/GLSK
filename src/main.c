e796f105580d7a06f60084552ccdecf84787827d
emanuele <me.emanuele.xyz@gmail.com>
2024-08-15 00:07:27 +0200

    enable opengl debug output callback

--- src/main.c
+++ src/main.c
@@ -13,6 +13,48 @@ static void glsk_glfw_error_callback(int error_code, const char* description)
     printf("[GLFW]: (%d) %s\n", error_code, description);
 }
 
+static void APIENTRY glsk_gl_debug_msg_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+    const char* default_str = "UNKNOWN";
+
+    // TODO: to be implemented
+    const char* source_str = default_str;
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API: { source_str = "API"; } break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM: { source_str = "WINDOW_SYSTEM"; } break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: { source_str = "SHADER_COMPILER"; } break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY: { source_str = "THIRD_PARTY"; } break;
+    case GL_DEBUG_SOURCE_APPLICATION: { source_str = "APPLICATION"; } break;
+    case GL_DEBUG_SOURCE_OTHER: { source_str = "OTHER"; } break;
+    }
+
+    const char* type_str = default_str;
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR: { type_str = "ERROR"; } break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: { type_str = "DEPRECATED_BEHAVIOR"; } break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: { type_str = "UNDEFINED_BEHAVIOR"; } break;
+    case GL_DEBUG_TYPE_PORTABILITY: { type_str = "PORTABILITY"; } break;
+    case GL_DEBUG_TYPE_PERFORMANCE: { type_str = "PERFORMANCE"; } break;
+    case GL_DEBUG_TYPE_MARKER: { type_str = "MARKER"; } break;
+    case GL_DEBUG_TYPE_PUSH_GROUP: { type_str = "PUSH_GROUP"; } break;
+    case GL_DEBUG_TYPE_POP_GROUP: { type_str = "POP_GROUP"; } break;
+    case GL_DEBUG_TYPE_OTHER: { type_str = "OTHER"; } break;
+    }
+
+    const char* severity_str = default_str;
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH: { severity_str = "HIGH"; } break;
+    case GL_DEBUG_SEVERITY_MEDIUM: { severity_str = "MEDIUM"; } break;
+    case GL_DEBUG_SEVERITY_LOW: { severity_str = "LOW"; } break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: { severity_str = "NOTIFICATION"; } break;
+    }
+
+    printf("[GL][SRC:%s][TYPE:%s][LEVEL:%s]: (%u) %s\n", source_str, type_str, severity_str, id, message);
+}
+
 int main(void)
 {
     glfwSetErrorCallback(glsk_glfw_error_callback);
@@ -27,6 +69,9 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #if defined(GLSK_DEBUG)
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+    #endif
     GLFWwindow* window = glfwCreateWindow(WINDOW_W, WINDOW_H, WINDOW_TITLE, NULL, NULL);
     if (!window)
     {
@@ -42,6 +87,24 @@ int main(void)
         puts("[GLSK]: failed to initialize GLAD");
         goto exit;
     }
+
+    #if defined(GLSK_DEBUG)
+    // NOTE: check for opengl debug context
+    {
+        int flags = 0;
+        glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+        if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+        {
+            glEnable(GL_DEBUG_OUTPUT);
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+            glDebugMessageCallback(glsk_gl_debug_msg_callback, NULL);
+        }
+        else
+        {
+            puts("[GLSK]: failed to create opengl debug context");
+        }
+    }
+    #endif
 
     while (!glfwWindowShouldClose(window))
     {
